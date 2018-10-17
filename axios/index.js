e22d8252b4659befcda9653f00ebac7969c341e6
Silver·湫澲 <autumnsilverleaves@gmail.com>
2018-10-17 17:55:07 +0800

    新增Vue的一些用法

--- axios/index.js
+++ axios/index.js
@@ -2,17 +2,19 @@ var axios = require("axios")
 
 async function login() {
     try {
-        var formData = {
+        let formData = {
             username: "abc@qq.com",
             password: "123456",
         }
-        var formdataString = '';
+
+        let formdataString = '';
         for (const key in formData) {
             if (formData.hasOwnProperty(key)) {
                 const element = formData[key];
                 formdataString += key + "=" + element + "&";
             }
         }
+
         let response = await axios.post(
             "http://106.14.219.21:8082/userLogin",
             formdataString, {
@@ -20,9 +22,11 @@ async function login() {
                     'Content-Type': 'application/x-www-form-urlencoded',
                 },
             })
+        // console.log(response);
         console.log(response.data);
     } catch (error) {
         console.log(error);
     }
-}
-login()
+};
+
+login();
