b9cef015f077eb162d4a68182fdba60b9480f005
SilverLeaves <autumnsilverleaves@gmail.com>
2019-08-19 17:38:27 +0800

    移除不必要的代码

--- grapql/qs.ts
+++
@@ -1,9 +0,0 @@
-import * as qs from "qs"
-
-let user = { userId: null, userName: "", sex: "male", school: { name: "jialidun", teacher: [{ name: "laowang" }, { name: "xiongmaoren" }] } }
-
-let code=qs.stringify(user)
-let decode=qs.parse(code)
-console.log(code)
-
-console.log(decode)
