c2d3fef66a5a4886ee3e2e7bfd7c073d9cba8191
SilverLeaves <autumnsilverleaves@gmail.com>
2019-08-22 19:15:52 +0800

    完善书和作者的关联关系

--- grapql/apollo.ts
+++ grapql/apollo.ts
@@ -3,16 +3,31 @@ const {
     gql
   } = require('apollo-server');
   
-  const books = [{
+  const books = [
+    {
       _id: "0",
-      title: 'Harry Potter and the Chamber of Secrets',
+      title: '哈利波特',
       author: {
         _id: "0",
       }
     },
     {
       _id: "1",
-      title: 'Jurassic Park',
+      title: '神器留下',
+      author: {
+        _id: "1",
+      }
+    },
+    {
+      _id: "2",
+      title: '热爱生命',
+      author: {
+        _id: "0",
+      }
+    },
+    {
+      _id: "3",
+      title: '哆啦A萌',
       author: {
         _id: "1",
       }
@@ -21,16 +36,22 @@ const {
   
   const authors = [{
       _id: "0",
-      name: 'J.K. Rowling',
+      name: '第一作者',
       books: [{
         _id: "0"
+      },
+      {
+        _id: "2"
       }]
     },
     {
       _id: "1",
-      name: 'Michael Crichton',
+      name: '第二作者',
       books: [{
         _id: "1"
+      },
+      {
+        _id: "3"
       }]
     },
   ];
