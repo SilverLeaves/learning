33e9df677e63dc8ad6767bf1d3927179fedff169
SilverLeaves <autumnsilverleaves@gmail.com>
2019-09-20 19:27:17 +0800

    修改

--- grapql/index.ts
+++ grapql/index.ts
@@ -1,24 +1,111 @@
-import * as gql from "graphql"
-
-// serve side
-let root = {
-    hello: () => {
-        return "Hello world"
+const {
+    ApolloServer,
+    gql
+  } = require('apollo-server');
+  
+  const books = [
+    {
+      _id: "0",
+      title: '哈利波特',
+      author: {
+        _id: "0",
+      }
+    },
+    {
+      _id: "1",
+      title: '神器留下',
+      author: {
+        _id: "1",
+      }
+    },
+    {
+      _id: "2",
+      title: '热爱生命',
+      author: {
+        _id: "0",
+      }
+    },
+    {
+      _id: "3",
+      title: '哆啦A萌',
+      author: {
+        _id: "1",
+      }
+    },
+  ];
+  
+  const authors = [{
+      _id: "0",
+      name: '第一作者',
+      books: [{
+        _id: "0"
+      },
+      {
+        _id: "2"
+      }]
+    },
+    {
+      _id: "1",
+      name: '第二作者',
+      books: [{
+        _id: "1"
+      },
+      {
+        _id: "3"
+      }]
+    },
+  ];
+  
+  const typeDefs = gql `
+  type Author{
+    _id:String
+    name:String
+    books:[Book]
+  }
+  
+    type Book {
+      title: String
+      author: [Author]
     }
-};
-
-
-// client side
-let schema = gql.buildSchema(`
-type Query{
-    hello:String
-}
-`);
-console.log("schema",schema);
-
-
-// query
-(async function query() {
-    let res = await gql.graphql(schema, `{hello}`, root);
-    console.log(res)
-})()
+  
+    # The "Query" type is the root of all GraphQL queries.
+    # (A "Mutation" type will be covered later on.)
+    type Query {
+      books: [Book]
+    }
+  `;
+  
+  const resolvers = {
+    Query: {
+      books: () => books,
+    },
+    Book: {
+      author: (parent, {}, context) => {
+        return authors.filter((ele) => {
+          return ele._id == parent.author._id
+        })
+      }
+    },
+    Author: {
+      books: (parent, {}, context) => {
+        let searcher = new Set();
+        parent.books.forEach(element => {
+          searcher.add(element._id)
+        });
+        return books.filter((ele) => {
+          return searcher.has(ele._id)
+        })
+      }
+    },
+  };
+  
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+  
+  server.listen().then(({
+    url
+  }) => {
+    console.log(`${url}`);
+  });
